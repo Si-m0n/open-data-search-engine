16fe5884abcb6a6629b9870ae29868ebe9e286ed
Si-m0n <simondefradas@gmail.com>
2022-12-27 17:28:11 +0100

    Added LegalBody and Decision in the database, enabled admin modifications on the database, added a base view.

--- search/datasearch/admin.py
+++ search/datasearch/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from datasearch.models import Decision, LegalBody
+
 # Register your models here.
+
+
+class DecisionAdmin(admin.ModelAdmin):
+    list_display = ("file_number", "legal_body")
+
+
+class LegalBodyAdmin(admin.ModelAdmin):
+    list_display = ("legal_name", "legal_code")
+
+
+admin.site.register(Decision, DecisionAdmin)
+admin.site.register(LegalBody, LegalBodyAdmin)
--- search/datasearch/models.py
+++ search/datasearch/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+class LegalBody(models.Model):
+    class LegalType(models.TextChoices):
+        COURS_ADMINISTRATIVE_APPEL = "CAA"
+        CONSEIL_ETAT = "CE"
+        TRIBUNAL_ADMINISTRATIF = "TA"
+
+    legal_type = models.CharField(
+        "Type de juridiction", null=True, choices=LegalType.choices, max_length=20
+    )
+    legal_name = models.CharField("Nom de la juridiction", max_length=100)
+    legal_code = models.CharField("Code de la juridiction", max_length=20)
+
+    def __str__(self):
+        return self.legal_code
+
+
+class Decision(models.Model):
+    file_number = models.IntegerField("Numéro de dossier")
+    last_update = models.DateField("Date de mise à jour")
+    lecture_date = models.DateField("Date de lecture")
+    lawyer = models.CharField("Avocat requérant", max_length=30)
+    decision_type = models.CharField("Type de décision", max_length=100)
+    appeal_type = models.CharField("Type de recours", max_length=100)
+    publication_code = models.CharField("Code publication", max_length=20)
+    solution = models.CharField("Solution", default="", blank=True, max_length=100)
+    text = models.CharField("Texte intégral", max_length=100000)
+
+    legal_body = models.ForeignKey(
+        LegalBody,
+        verbose_name="Instance judiciaire",
+        null=True,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return str(self.file_number)
